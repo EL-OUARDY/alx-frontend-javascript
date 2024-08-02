01b091ca3003194b42d4c654c208d71be04c2e0e
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-08-02 20:38:46 +0100

    FIX: Task 6

--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,3 +1,10 @@
 import handleProfileSignup from './6-final-user';
 
-console.log(handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg'));
+// console.log(handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg'));
+
+async function test() {
+  const queue = await handleProfileSignup('John', 'Doe', 'Gerald.jpg');
+  console.log(queue);
+}
+
+test();
